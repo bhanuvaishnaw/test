180204e2f98a316aca39a50e47adc0cc0ff1459f
bhanuvaishnaw <bhanu.vaishnaw@gmail.com>
2019-03-25 15:05:11 +0530

    Pom changes

--- src/main/java/com/example/boot/HelloWorldApplication.java
+++ src/main/java/com/example/boot/HelloWorldApplication.java
@@ -1,16 +1,23 @@
 package com.example.boot;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class HelloWorldApplication {
 
+	@SuppressWarnings("unused")
+	private static ConfigurableApplicationContext app;
+	
 	public static void main(String[] args) {
-		SpringApplication.run(HelloWorldApplication.class, args);
+		app = SpringApplication.run(HelloWorldApplication.class, args);
 		
 		System.out.println("Hello world");
 		String mvnversion = executeCommand("mvn --version");
@@ -20,6 +27,18 @@ public class HelloWorldApplication {
 		System.out.println("mvn version: "+mvnversion);
 		System.out.println("java version: "+javaversion);
 		System.out.println("git version: "+gitversion);
+		
+		SimpleDateFormat formattr = new SimpleDateFormat("dd-MMM-yy");
+		String tdaysDt = formattr.format(new Date());
+		Date systemDt;
+		try {
+			systemDt = formattr.parse(tdaysDt);
+			System.out.println(tdaysDt);
+			System.out.println(systemDt);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	public static String executeCommand(String command) {
